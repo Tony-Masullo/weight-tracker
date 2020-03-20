e070bc75a947290a13654173d3ef143f65eca740
Tony-Masullo <tonymasullo5@gmail.com>
2020-03-20 18:46:13 -0400

    finished basic layout/design

--- src/app/weight-input/weight-input.component.ts
+++ src/app/weight-input/weight-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-weight-input',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeightInputComponent implements OnInit {
 
+  //LineChart = [];
+  chart: any;
+  //dataPoints: [9, 7, 3, 5, 2, 10, 15, 16, 19, 3, 1, 9];
+ // labels: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"], // your labels array
+        datasets: [
+          {
+            data: [9, 7, 3, 5, 2, 10, 15, 16, 19, 3, 1, 9], // your data array
+            borderColor: 'red',
+            fill: false
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }],
+        }
+      }
+    });
   }
-
 }
