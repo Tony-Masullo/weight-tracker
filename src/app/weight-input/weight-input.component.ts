9d5a187ce320535a81395e64538098bdaa099360
Tony-Masullo <tonymasullo5@gmail.com>
2020-03-21 22:30:26 -0400

    updated functionality to add weight; shows graph of weight versus date

--- src/app/weight-input/weight-input.component.ts
+++ src/app/weight-input/weight-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-weight-input',
@@ -8,28 +9,35 @@ import { Chart } from 'chart.js';
 })
 export class WeightInputComponent implements OnInit {
 
-  chart: any;
+  weightChart: any;
+  bodyFatChart: any;
 
-  weightToAdd = '';
-  bodyFatToAdd = 0;
-  dateToAdd = '';
+   weightToAdd = '';
+   bodyFatToAdd = '';
+  // dateToAdd = '';
+  increment = 0;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.chart = new Chart('canvas', {
+    this.weightChart = new Chart('weightCanvas', {
       type: 'line',
       data: {
         labels: [], // your labels array
         datasets: [
           {
-            data: [], // your data array
-            borderColor: 'red',
+            data: [], // your weight data array
+            borderColor: 'blue',
             fill: false
           }
         ]
       },
       options: {
+        maintainAspectRatio: false,
+        title: {
+          display: true,
+          text: 'Weight Over Time (lbs)'
+        },
         legend: {
           display: false
         },
@@ -40,21 +48,63 @@ export class WeightInputComponent implements OnInit {
           yAxes: [{
             display: true
           }],
+        },
+        layout: {
+          padding: {
+            left: 30
+          }
         }
       }
     });
+
+    // this.bodyFatChart= new Chart('bodyFatCanvas', {
+    //   type: 'line',
+    //   data: {
+    //     labels: [], // your labels array
+    //     datasets: [
+    //       {
+    //         data: [], // your body Fat data array
+    //         borderColor: 'red',
+    //         fill: false
+    //       }
+    //     ]
+    //   },
+    //   options: {
+    //     maintainAspectRatio: false,
+    //     title: {
+    //       display: true,
+    //       text: 'Body Fat Over Time (%)'
+    //     },
+    //     legend: {
+    //       display: false
+    //     },
+    //     scales: {
+    //       xAxes: [{
+    //         display: true
+    //       }],
+    //       yAxes: [{
+    //         display: true
+    //       }],
+    //     },
+    //     layout: {
+    //       padding: {
+    //         left: 50
+    //       }
+    //     }
+    //   }
+    // });
   }
 
-  public onKey(event: any) { // without type info
-    this.weightToAdd = event.target.value;
-  }
+  public submitWeight(weight, bodyFat, date){
+   
+    console.log(weight, bodyFat, date);
+    this.weightChart.data.labels.push(date);
+    this.weightChart.data.datasets[0].data.push(weight);
+    this.weightChart.update();
 
-  public addData() {
-    this.chart.data.labels.push(this.bodyFatToAdd++);
-    this.chart.data.datasets.forEach((dataset) => {
-      dataset.data.push(this.weightToAdd);
-    });
-    this.chart.update();
-    this.weightToAdd = '';
+    // this.bodyFatChart.data.labels.push(date);
+    // this.bodyFatChart.data.datasets[0].data.push(bodyFat);
+    // this.bodyFatChart.update();
   }
+  
 }
